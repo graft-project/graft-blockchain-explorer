9acd9e79bd51b9db1b25b0a11ee908421b9dfe36
Ilya Kitaev <mbg033@gmail.com>
2017-12-08 18:01:32 +0300

    fixed startup script

--- start-on-testnet.sh
+++ start-on-testnet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # src/templates directory needs to be copied to current directory
 
-xmrblocks --bc-path <path-to-node-data-dir>/lmdb \
+grfblocks --bc-path <path-to-node-data-dir>/lmdb \
     --testnet \
-    --deamon-url=52.88.171.144:28981 \
+    --deamon-url=localhost:28981 \
     --enable-emission-monitor
 
